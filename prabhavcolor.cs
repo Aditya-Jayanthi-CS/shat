cae71214625ff88776b4e8ee4fec471397e9f26c
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2024-12-13 15:09:05 -0800

    Update prabhavcolor.cs

--- prabhavcolor.cs
+++ prabhavcolor.cs
@@ -2,14 +2,17 @@
   {   
       public static void GenerateColor(int minRed, int maxRed, int minGreen, int maxGreen, int minBlue, int maxBlue) 
       { 
-          Random random = new Random(); 
-          int red = random.Next(minRed, maxRed + 1); 
-          int green = random.Next(minGreen, maxGreen + 1); 
-          int blue = random.Next(minBlue, maxBlue + 1); 
+          Random random = new Random();
+
+          for (int i = 0; i < count; i + +)
+          {
+              int red = random.Next(minRed, maxRed + 1); 
+              int green = random.Next(minGreen, maxGreen + 1); 
+              int blue = random.Next(minBlue, maxBlue + 1); 
   
-          Color randomColor = Color.FromArgb(red, green, blue); 
-          Console.WriteLine($"Random color for maze walls: #{randomColor.R:X2}{randomColor.G:X2}{randomColor.B:X2}");
-      }
+              Color randomColor = Color.FromArgb(red, green, blue); 
+              Console.WriteLine($"Random color for maze walls: #{randomColor.R:X2}{randomColor.G:X2}{randomColor.B:X2}");
+          }
 
       public static void Main()
       {
