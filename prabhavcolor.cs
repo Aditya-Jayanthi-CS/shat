50183ef5782bf8c97ad4c3753db6d3ff140b5ec8
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2024-12-17 14:40:13 -0800

    Update prabhavcolor.cs

--- prabhavcolor.cs
+++ prabhavcolor.cs
@@ -11,8 +11,7 @@
               int blue = random.Next(minBlue, maxBlue + 1); 
   
               Color randomColor = Color.FromArgb(red, green, blue); 
-              Console.WriteLine($"Random color for maze walls: #{randomColor.R:X2}{randomColor.G:X2}{randomColor.B:X2}");
-
+            
               if (renderer != null) 
               {
                   renderer.material.color = randomColor;
